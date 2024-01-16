6fcd14da7717b5b70ac2104b352d5b5896c494df
KatynOCtraining <t.theveupolaris@gmail.com>
2024-01-16 23:58:50 +0100

    fix styles 4/4

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,10 +1,65 @@
+import React, { useState, useEffect } from "react";
 import "../styles/banner.css";
 
-export default function Banner({ image, title }) {
+const Banner = ({ image, title }) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 767);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const splitTitle = (title) => {
+    const words = title.split(" ");
+    const lines = [];
+    let currentLine = "";
+
+    words.forEach((word, index) => {
+      const proposedLine = currentLine + (currentLine ? " " : "") + word;
+
+      if (proposedLine.length <= 15) {
+        currentLine = proposedLine;
+      } else {
+        lines.push(currentLine);
+        currentLine = word;
+      }
+
+      if (index === words.length - 1) {
+        lines.push(currentLine);
+      }
+    });
+
+    if (lines.length > 2) {
+      const firstLine = lines.slice(0, 2).join(" ");
+      const secondLine = lines.slice(2).join(" ");
+      return [firstLine, secondLine];
+    }
+
+    return lines;
+  };
+
   return (
-    <div className="banner_container">
+    <div className={`banner_container ${isMobile ? "mobile" : ""}`}>
       <img className="banner-img" src={image} alt="banner" />
-      <h1 className="banner-title">{title}</h1>
+      <h1 className="banner-title">
+        {isMobile
+          ? splitTitle(title).map((line, index) => (
+              <React.Fragment key={index}>
+                {line}
+                {index < splitTitle(title).length - 1 && <br />}
+              </React.Fragment>
+            ))
+          : title}
+      </h1>
     </div>
   );
-}
+};
+
+export default Banner;
--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,12 +1,13 @@
+import React from "react";
 import Banner from "../components/banner";
 import banner from "../assets/banner-home.png";
 import Card from "../components/card";
 import accomodations from "../datas/data.json";
 
-export default function Accueil() {
+const Accueil = () => {
   return (
     <>
-      <Banner image={banner} title="Chez vous, partout et ailleurs" />
+      <Banner image={banner} title="Chez vous , partout et ailleurs" />
       <section className="card-container">
         {accomodations.map((apart) => (
           <Card key={apart.id} data={apart} />
@@ -14,4 +15,6 @@ export default function Accueil() {
       </section>
     </>
   );
-}
+};
+
+export default Accueil;
